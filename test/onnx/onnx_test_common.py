f12f79dcbdd272372d9ac0625bd879b17c9c759f
BowenBao <bowbao@microsoft.com>
2023-08-10 11:44:18 -0700

    [ONNX] Public diagnostic options for 'dynamo_export'
    ghstack-source-id: d8b7c5d18361b6b6c5fbd3e2a59281223ebffb51
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/106741

--- test/onnx/onnx_test_common.py
+++ test/onnx/onnx_test_common.py
@@ -262,30 +262,49 @@ class _TestONNXRuntime(pytorch_test_common.ExportTestCase):
             ref_input_args = input_args
             ref_input_kwargs = input_kwargs
 
-        # Feed args and kwargs into exporter.
-        # Note that exporter should flatten kwargs into positional args the exported model;
-        # since ONNX doesn't represent kwargs.
-        export_output = torch.onnx.dynamo_export(
-            ref_model,
-            *ref_input_args,
-            **ref_input_kwargs,
-            export_options=torch.onnx.ExportOptions(
-                op_level_debug=self.op_level_debug,
-                dynamic_shapes=self.dynamic_shapes,
-            ),
-        )
-
-        if not skip_dynamic_shapes_check:
-            assert_dynamic_shapes(export_output, self.dynamic_shapes)
-
-        if verbose:
-            export_output.diagnostic_context.dump(
+        sarif_log_path = (
+            (
                 f"test_report_{self._testMethodName}"
                 f"_op_level_debug_{self.op_level_debug}"
                 f"_dynamic_axes_{self.dynamic_shapes}"
-                ".sarif",
-                compress=False,
+                ".sarif"
             )
+            if verbose
+            else None
+        )
+
+        # Feed args and kwargs into exporter.
+        # Note that exporter should flatten kwargs into positional args the exported model;
+        # since ONNX doesn't represent kwargs.
+        export_error: Optional[torch.onnx.OnnxExporterError] = None
+        try:
+            export_output = torch.onnx.dynamo_export(
+                ref_model,
+                *ref_input_args,
+                **ref_input_kwargs,
+                export_options=torch.onnx.ExportOptions(
+                    op_level_debug=self.op_level_debug,
+                    dynamic_shapes=self.dynamic_shapes,
+                    diagnostic_sarif_log_path=sarif_log_path,
+                ),
+            )
+        except torch.onnx.OnnxExporterError as e:
+            export_error = e
+            export_output = e.export_output
+
+        if verbose:
+            export_output.save_sarif_log(
+                f"test_report_{self._testMethodName}"
+                f"_op_level_debug_{self.op_level_debug}"
+                f"_dynamic_axes_{self.dynamic_shapes}"
+                ".sarif"
+            )
+
+        if export_error is not None:
+            raise export_error
+
+        if not skip_dynamic_shapes_check:
+            assert_dynamic_shapes(export_output, self.dynamic_shapes)
 
         _compare_pytorch_onnx_with_ort(
             export_output,
